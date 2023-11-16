cb4c119b6081dd17ec7bf521e635850f665c9600
GET Academy <terje@kolderup.net>
2023-11-16 12:57:12 +0100

    Initial commit

---
+++ SimpleGameOfLife/Game.cs
@@ -0,0 +1,41 @@
+﻿using Console = System.Console;
+
+namespace SimpleGameOfLife
+{
+    internal class Game
+    {
+        public static void Run()
+        {
+            var coloumnCount = Console.WindowWidth;
+            var rowCount = Console.WindowHeight - 1;
+            var cellCount = coloumnCount * rowCount;
+
+            var random = new Random();
+            var cells = new char[cellCount];
+            for (int i = 0; i < cells.Length; i++)
+            {
+                var randomNumber0or1 = random.Next(0, 2);
+                var isAlive = randomNumber0or1 == 1;
+                cells[i] = isAlive ? 'O' : ' ';
+            }
+
+            Console.WriteLine(cells);
+        }
+
+        /*
+           var name = Console.ReadLine();
+           if(name.Length>0) Greet(name);
+           else Greet();
+         */
+
+        private static void Greet(string name)
+        {
+            Console.WriteLine($"Hei, {name}!");
+        }
+
+        private static void Greet()
+        {
+            Console.WriteLine($"Hei på deg!");
+        }
+    }
+}
---
+++ SimpleGameOfLife/Program.cs
@@ -0,0 +1,35 @@
+﻿using SimpleGameOfLife;
+
+Game.Run();
+
+/*
+
+Pause til 13:03
+
+
+    Mål: "Bli komfortabel med C#" - mestre det dere kan fra JavaScript i C#
+
+    Noen ting som er annerledes: 
+     - arrays
+     - overloads og default parametre
+     - casting, parse og enum
+     - klassevariable
+
+    Game of life
+    "simulering av celler, liv"
+
+     -  Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+     -  Any live cell with two or three live neighbours lives on to the next generation.
+     -  Any live cell with more than three live neighbours dies, as if by overpopulation.
+     -  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
+    1: Modell
+    2: Fylle modell med tilfeldige data
+    3: Vise alle cellene (view)
+    4: Loope og endre tilfeldig
+    5: Endre ut fra reglene
+       - telle naboer
+       - bestemme om hver celle skal leve eller ikke i neste generasjon
+       
+    
+*/
